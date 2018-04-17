9f27732b613bf7cdcb890828c5ff095ed76b03f3
Mojo <35627917+Mojo-LB@users.noreply.github.com>
2018-04-18 01:41:07 +0300

    Update deploy.bash

--- deployment/deploy.bash
+++ deployment/deploy.bash
@@ -6,7 +6,7 @@ if [[ `whoami` == "root" ]]; then
     echo "You ran me as root! Do not run me as root!"
     exit 1
 fi
-ROOT_SQL_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+ROOT_SQL_PASS=MojoMojo
 CURUSER=$(whoami)
 sudo timedatectl set-timezone Etc/UTC
 sudo apt-get update
@@ -34,39 +34,6 @@ npm install -g pm2
 openssl req -subj "/C=IT/ST=Pool/L=Daemon/O=Mining Pool/CN=test.dero.pro" -newkey rsa:2048 -nodes -keyout cert.key -x509 -out cert.pem -days 36500
 sudo mkdir /pool_db/
 sudo chmod 7777 -R /pool_db
-sed -r "s/(\"db_storage_path\": ).*/\1\"\/home\/$CURUSER\/pool_db\/\",/" config_example.json > config.json
-cd ~
-git clone https://github.com/mesh0000/poolui.git
-cd poolui
-npm install
-./node_modules/bower/bin/bower update
-./node_modules/gulp/bin/gulp.js build
-cd build
-sudo ln -s `pwd` /var/www
-CADDY_DOWNLOAD_DIR=$(mktemp -d)
-cd $CADDY_DOWNLOAD_DIR
-curl -sL "https://snipanet.com/caddy.tar.gz" | tar -xz caddy init/linux-systemd/caddy.service
-sudo mv caddy /usr/local/bin
-sudo chown root:root /usr/local/bin/caddy
-sudo chmod 755 /usr/local/bin/caddy
-sudo setcap 'cap_net_bind_service=+ep' /usr/local/bin/caddy
-sudo groupadd -g 33 www-data
-sudo useradd -g www-data --no-user-group --home-dir /var/www --no-create-home --shell /usr/sbin/nologin --system --uid 33 www-data
-sudo mkdir /etc/caddy
-sudo chown -R root:www-data /etc/caddy
-sudo mkdir /etc/ssl/caddy
-sudo chown -R www-data:root /etc/ssl/caddy
-sudo chmod 0770 /etc/ssl/caddy
-sudo cp ~/nodejs-pool/deployment/caddyfile /etc/caddy/Caddyfile
-sudo chown www-data:www-data /etc/caddy/Caddyfile
-sudo chmod 444 /etc/caddy/Caddyfile
-sudo sh -c "sed 's/ProtectHome=true/ProtectHome=false/' init/linux-systemd/caddy.service > /etc/systemd/system/caddy.service"
-sudo chown root:root /etc/systemd/system/caddy.service
-sudo chmod 644 /etc/systemd/system/caddy.service
-sudo systemctl daemon-reload
-sudo systemctl enable caddy.service
-sudo systemctl start caddy.service
-rm -rf $CADDY_DOWNLOAD_DIR
 cd ~
 sudo env PATH=$PATH:`pwd`/.nvm/versions/node/v8.9.3/bin `pwd`/.nvm/versions/node/v8.9.3/lib/node_modules/pm2/bin/pm2 startup systemd -u $CURUSER --hp `pwd`
 cd ~/nodejs-pool
